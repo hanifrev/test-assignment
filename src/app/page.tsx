15c990447f2dd5f19849364e70724f22dff2c577
Faishal Hanif <hanifrev@gmail.com>
2024-10-12 07:06:00 +0700

    fix errror

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,28 @@
-import fetcher from '@/common/utils/fetcher';
 import MainPage from '@/modules/MainPage/page';
 import { mainPageType } from '@/modules/MainPage/types/mainPage.models';
 
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_API_URL;
+
+const getData = async () => {
+  const response = await fetch(`${BASE_URL}/api/mainPage`);
+
+  if (!response.ok) {
+    throw new Error('ERROR while getting data');
+  }
+
+  const result = await response.json();
+  return result;
+};
+
 export default async function Home() {
-  const getData = await fetcher<mainPageType>({
-    path: '/mainPage',
-    isExternal: false,
-  });
+  if (!BASE_URL) {
+    return null;
+  }
 
-  console.log(getData);
+  const data: mainPageType = await getData();
 
-  return <MainPage data={getData} />;
+  console.log(data);
+
+  //@ts-ignore
+  return <MainPage data={data} />;
 }
